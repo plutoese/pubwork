69d946649b4fbbbee46ad91d50e17ef18a6b1d66
DESKTOP-0V5S9Q0\plutoese <glen.zhang7@gmail.com>
2017-09-02 17:21:35 +0800

    notebook runner

--- pubwork/application/cnkiscraper/app_cnkiscrpaer.py
+++ pubwork/application/cnkiscraper/app_cnkiscrpaer.py
@@ -7,18 +7,14 @@
 # --------------------------------------
 
 
-import time
-from libs.multithread.class_multithread import MultiThread
-from libs.webscraper.class_proxiesmanager import ProxyManager
+from sheldon.webscraper.class_proxymanager import ProxyManager
 from application.cnkiscraper.class_cnkiscraper import CnkiScraperInterface
+import concurrent.futures
 
 
 # 0. 函数
 def scraper(journal, year, proxy_manager=None):
-    if proxy_manager is not None:
-        cnki_interface = CnkiScraperInterface(proxy=proxy_manager.top_50_proxies)
-    else:
-        cnki_interface = CnkiScraperInterface()
+    cnki_interface = CnkiScraperInterface(proxy=proxy_manager.top_50_proxies)
 
     literatures = cnki_interface.query(querystr="JN='{}'".format(journal),
                                        period={'start_year': year, 'end_year': year})
@@ -28,36 +24,11 @@ def scraper(journal, year, proxy_manager=None):
 
 # 1. 初始化设置
 JOURNALS = ['经济研究','经济学(季刊)','金融研究','世界经济','中国工业经济','产业经济研究','经济科学','经济学家']
-JOURNAL = JOURNALS[2]
-YEAR = [str(year) for year in range(2000,2005)]
+JOURNAL = JOURNALS[3]
+YEAR = [str(year) for year in range(2000,2017)]
 
 proxy_manager = ProxyManager()
 
-# 2. 抓取文献数据
-threads = []
-
-for y in YEAR:
-    t = MultiThread(scraper, args=(JOURNAL,y,proxy_manager), name=y)
-    threads.append(t)
-
-for thread in threads:
-    thread.start()
-
-for thread in threads:
-    thread.join()
-
-'''
-def good():
-    sign = True
-    while sign:
-        try:
-            print('I am here!')
-            bad()
-            return 24
-        except Exception:
-            print('continue!')
-
-def bad():
-    return 24/0
-
-print(good())'''
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    for y in YEAR:
+        executor.submit(scraper, JOURNAL, y, proxy_manager)
---
+++ pubwork/sheldon/notebook/class_notebookrunner.py
@@ -0,0 +1,19 @@
+import io
+from nbformat import read
+from IPython.core.interactiveshell import InteractiveShell
+
+
+def run_notebook(path):
+    shell = InteractiveShell.instance()
+    with io.open(path, 'r', encoding='utf-8') as f:
+                nb = read(f, 4)
+
+    result = []
+    for cell in nb.cells:
+        if cell.cell_type == 'code':
+            # transform the input to executable Python
+            code = shell.input_transformer_manager.transform_cell(cell.source)
+            # run the code in themodule
+        result.append(shell.run_cell(code))
+
+    return result
